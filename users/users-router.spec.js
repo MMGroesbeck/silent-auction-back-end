f0dad7c58ef2bf45e5cbe39dac03c7357d79903e
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-27 14:10:32 -0600

    tests for users endpoints

--- users/users-router.spec.js
+++ users/users-router.spec.js
@@ -31,4 +31,36 @@ describe("testing users endpoints: login and register", () => {
                 expect(res.status).toBe(500);
             });
         });
+    test("should allow registration with same email but different role", () => {
+        return request(server)
+            .post("/api/users/register")
+            .send({ password: "mila", username: "lyudmila", email: "mila@gmail.com", role: "seller" })
+            .then((res) => {
+                expect(res.status).toBe(201);
+            });
+        });
+    test("can login", () => {
+        return request(server)
+            .post("/api/users/login")
+            .send({ password: "mila", username: "mila"})
+            .then((res) => {
+                expect(res.status).toBe(200);
+            });
+    });
+    test("can't login if password is incorrect", () => {
+        return request(server)
+            .post("/api/users/login")
+            .send({ password: "mila1", username: "mila"})
+            .then((res) => {
+                expect(res.status).toBe(401);
+            });
+    });
+    test("can't login if username is incorrect", () => {
+        return request(server)
+            .post("/api/users/login")
+            .send({ password: "mila", username: "mila1"})
+            .then((res) => {
+                expect(res.status).toBe(401);
+            });
+    });
 });
