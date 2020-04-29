627c8697c8f770dc0303eda0024b46d641a8554c
M Groesbeck <m.groesbeck@gmail.com>
2020-04-29 09:54:51 -0700

    Timestamp tools check production vs development before making comparison.

--- api/compare-timestamps.js
+++ api/compare-timestamps.js
@@ -9,36 +9,40 @@ module.exports = {
 
 function setCompleted(id){
     const rightnow = new Date();
+    const comparison = (process.env.DB_ENV == "production") ? rightnow.toISOString() : Date.now();
     return db("auctions")
         .where("id", id)
         .andWhere("status", "active")
-        .andWhere("end_datetime", "<", rightnow.toISOString())
+        .andWhere("end_datetime", "<", comparison)
         .update({status: "completed"});
 }
 
 function setCompletedArr(ids){
     const rightnow = new Date();
+    const comparison = (process.env.DB_ENV == "production") ? rightnow.toISOString() : Date.now();
     return db("auctions")
         .whereIn("id", ids)
         .andWhere("status", "active")
-        .andWhere("end_datetime", "<", rightnow.toISOString())
+        .andWhere("end_datetime", "<", comparison)
         .update({status: "completed"});
 }
 
 function setCompletedAll(){
     const rightnow = new Date();
+    const comparison = (process.env.DB_ENV == "production") ? rightnow.toISOString() : Date.now();
     return db("auctions")
         .where("status", "active")
-        .andWhere("end_datetime", "<", rightnow.toISOString())
+        .andWhere("end_datetime", "<", comparison)
         .update({status: "completed"});
 }
 
 function checkExpired(id){
     const rightnow = new Date();
+    const comparison = (process.env.DB_ENV == "production") ? rightnow.toISOString() : Date.now();
     return db("auctions")
         .where("id", id)
         .then(resp => {
-            if (resp[0].end_datetime < rightnow.toISOString()) {
+            if (resp[0].end_datetime < comparison) {
                 return true;
             } else {
                 return false;
