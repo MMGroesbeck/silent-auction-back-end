8b4754f6c6bc164d608658fd1b6ecaca8772c7b5
M Groesbeck <m.groesbeck@gmail.com>
2020-04-29 08:22:08 -0700

    Timestamp tools built in compare-timestamps.js but not yet implemented.

--- api/compare-timestamps.js
+++ api/compare-timestamps.js
@@ -8,28 +8,25 @@ module.exports = {
 }
 
 function setCompleted(id){
-    const rightnow = new Date();
     return db("auctions")
         .where("id", id)
         .andWhere("status", "active")
-        .andWhere("end_datetime", "<", rightnow.toISOString())
+        .andWhere("end_datetime", "<", Date.now())
         .update({status: "completed"});
 }
 
 function setCompletedArr(ids){
-    const rightnow = new Date();
     return db("auctions")
         .whereIn("id", ids)
         .andWhere("status", "active")
-        .andWhere("end_datetime", "<", rightnow.toISOString())
+        .andWhere("end_datetime", "<", Date.now())
         .update({status: "completed"});
 }
 
 function setCompletedAll(){
-    const rightnow = new Date();
     return db("auctions")
         .where("status", "active")
-        .andWhere("end_datetime", "<", rightnow.toISOString())
+        .andWhere("end_datetime", "<", Date.now())
         .update({status: "completed"});
 }
 
@@ -37,8 +34,7 @@ function checkExpired(id){
     return db("auctions")
         .where("id", id)
         .then(resp => {
-            const rightnow = new Date();
-            if (resp[0].end_datetime < rightnow.toISOString()) {
+            if (resp[0].end_datetime < Date.now()) {
                 return true;
             } else {
                 return false;
